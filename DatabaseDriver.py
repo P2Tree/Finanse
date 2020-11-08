5b4a857a6b5e7f628edd16c530f7d6c59f922642
P2Tree.MacBookPro <dicksonliuming@gmail.com>
2020-11-08 12:02:48 +0800

    Add account statistic of month and adjust account.

--- DatabaseDriver.py
+++ DatabaseDriver.py
@@ -1,36 +1,45 @@
-from DatabaseModel import User, Book, Bill, Account, AccountGroup
+from DatabaseModel import User, Book, Bill, Account, AccountGroup, AccountStatMonth
 from DatabaseModel import db
 import datetime
 
 class DatabaseDriver():
     def __init__(self):
         self.current_user = None
-        self.CreateDatabase()
+        self.create_database()
 
-    def CreateDatabase(self):
-        User.create_table()
-        Book.create_table()
-        Bill.create_table()
-        AccountGroup.create_table()
-        Account.create_table()
+    def getUser(self):
+        return self.current_user
 
-    def Login(self):
+    def create_database(self):
+        if not db.table_exists("user"):
+            User.create_table()
+        if not db.table_exists("book"):
+            Book.create_table()
+        if not db.table_exists("bill"):
+            Bill.create_table()
+        if not db.table_exists("accountgroup"):
+            AccountGroup.create_table()
+        if not db.table_exists("account"):
+            Account.create_table()
+        if not db.table_exists("accountstatmonth"):
+            AccountStatMonth.create_table()
+
+    def login(self):
         email = input("Info: Input login email: ")
         password = input("Info: Input password: ")
-        user = User.get_or_none(email = email, password = password)
+        user = User.get_or_none(email=email, password=password)
         if not user:
             print("Info: Not find you in the system.")
             ins = input("Info: Sign up? (y/n): ")
             if ins == 'y':
-                user = CreateUser()
+                self.create_user()
             else:
                 exit()
         else:
+            self.current_user = user
             print("Info: Welcome you, %s" % user.nickname)
 
-        self.current_user = user
-
-    def CreateUser(self):
+    def create_user(self):
         email = input("Info: Input login email: ")
         password = input("Info: Input password: ")
         valid_password = input("Info: Input same password again: ")
@@ -40,7 +49,7 @@ class DatabaseDriver():
             valid_password = input("Info: Input same password again: ")
 
         nickname = input("Info: Input nickname: ")
-        user = User.get_or_create(email = email, nickname = nickname, password = password)
+        user = User.get_or_create(email=email, nickname=nickname, password=password)
 
         if user[1]:
             print("Info: New user %s created." % nickname)
@@ -49,10 +58,10 @@ class DatabaseDriver():
 
         self.current_user = user[0]
 
-    def CreateBook(self, name):
+    def create_book(self, name):
         book_name = self.current_user.nickname + "的" + name
-        current_book = Book.get_or_create( \
-                book_name = book_name, is_default = True, user_id = self.current_user.id)
+        current_book = Book.get_or_create(
+                book_name=book_name, is_default=True, user_id=self.current_user.id)
 
         if current_book[1]:
             print("Info: New book %s created." % book_name)
@@ -61,18 +70,16 @@ class DatabaseDriver():
 
         return current_book[0]
 
-    def CreateTestBill(self, book):
-        with db.atomic():
-            Bill.insert_many([
-                (3.0, "支出", datetime.date(2020, 11, 1), datetime.time(8, 0, 0), "地铁", book.id, self.current_user.id),
-                (60.0, "支出", datetime.date(2020, 11, 1), datetime.time(10, 20, 0), "买菜", book.id, self.current_user.id)
-                ], ['amount', 'inout_type', 'billing_date', 'billing_time', 'comments', 'book_id', 'user_id']
-                ).execute()
+    def create_account(self, name, is_credit=False, group_name=None):
+        if not group_name:
+            group = AccountGroup.get_or_create(account_group_name="未分组")[0]
 
-    def CreateAccount(self, name, is_credit = False, group = None):
+        group = AccountGroup.get_or_none(account_group_name=group_name, user_id=self.current_user.id)
         if not group:
-            group = AccountGroup.get_or_create(account_group_name = "未分组")[0]
-        account = Account.get_or_create(account_name = name, is_credit = is_credit, user_id = self.current_user.id, account_group_id = group.id)
+            print("Warning: Account group %s is not existed." % group_name)
+            return None
+
+        account = Account.get_or_create(account_name=name, is_credit=is_credit, user_id=self.current_user.id, account_group_id=group.id, currency="Dollar")
 
         if account[1]:
             print("Info: New account %s created." % name)
@@ -81,8 +88,8 @@ class DatabaseDriver():
 
         return account[0]
 
-    def CreateAccountGroup(self, name, comments = None):
-        account_group = AccountGroup.get_or_create(account_group_name = name, comments = comments, user_id = self.current_user.id)
+    def create_account_group(self, name, comments=None):
+        account_group = AccountGroup.get_or_create(account_group_name=name, comments=comments, user_id=self.current_user.id)
 
         if account_group[1]:
             print("Info: New account group %s created." % name)
@@ -91,6 +98,59 @@ class DatabaseDriver():
 
         return account_group[0]
 
+    def create_account_stat_month(self, date, account_name, amount, adjust, interest_income,
+                                  invest_income, normal_income, normal_outcome, transfer):
+        account = Account.get_or_none(account_name=account_name, user_id=self.current_user.id)
+        if not account:
+            print("Warning: Account %s is not existed." % account_name)
+            return None
+
+        stat_month = AccountStatMonth.get_or_none(date=date, account_id=account.id)
+        if not stat_month:
+            stat_month = AccountStatMonth.create(date=date, account_id=account.id,
+                                                 amount=amount, adjust=adjust,
+                                                 interest_income=interest_income,
+                                                 invest_income=invest_income,
+                                                 normal_income=normal_income,
+                                                 normal_outcome=normal_outcome,
+                                                 transfer=transfer)
+            print("item is inserted.")
+        else:
+            print("item is already existed.")
+
+        print("[date: %s, account: %s, amount: %f, adjust: %f, interest_income: %f, invest_income: %f, normal_income: %f, normal_outcome: %f, transfer: %f]" %
+              (stat_month.date, account.account_name, stat_month.amount,
+               stat_month.adjust, stat_month.interest_income,
+               stat_month.invest_income, stat_month.normal_income,
+               stat_month.normal_outcome, stat_month.transfer))
+
+def CreateTestBill(user, book):
+    with db.atomic():
+        Bill.insert_many([
+            (3.0, "支出", datetime.date(2020, 11, 1), datetime.time(8, 0, 0), "地铁", book.id, user.id),
+            (60.0, "支出", datetime.date(2020, 11, 1), datetime.time(10, 20, 0), "买菜", book.id, user.id)
+            ], ['amount', 'inout_type', 'billing_date', 'billing_time', 'comments', 'book_id', 'user_id']
+            ).execute()
+
+def InitDatabase(driver):
+    # 创建账本
+    book = driver.create_book("日常账本")
+    # 创建账单条目
+    CreateTestBill(driver.getUser(), book)
+    # 创建账户分组
+    groups = []
+    groups.append(driver.create_account_group("住房基金", "用于住房支出"))
+    groups.append(driver.create_account_group("日常开支", "日常消费使用"))
+    # 创建账户，并指定分组
+    normal_accounts = []
+    credit_accounts = []
+    normal_accounts.append(driver.create_account("工商银行储蓄卡", False, "住房基金"))
+    credit_accounts.append(driver.create_account("浦发银行信用卡", True, "日常开支"))
+    # 创建账户月统计信息
+    # 日期、账户、余额、调整额、利息收入、投资收入、常规收入（>0）、常规支出（>0）、转账
+    driver.create_account_stat_month("2020-10-00", "工商银行储蓄卡", 1000.2, 0.0, 0.8, -10.0, 5000.0, 19.8, -20.0)
+    driver.create_account_stat_month("2020-10-00", "浦发银行信用卡", -110.5, 5.0, 0.0, 0.0, 0.0, 898.8, 20.0)
+
 if __name__ == "__main__":
     db.connect(reuse_if_open = True)
 
@@ -98,17 +158,13 @@ if __name__ == "__main__":
 
     ins = input("Sign in (1) or sign up (2): ")
     if ins == '1':
-        user = driver.Login()
+        driver.login()
     elif ins == '2':
-        user = driver.CreateUser()
+        driver.create_user()
     else:
         print("Error: Wrong input.")
         exit()
-    book = driver.CreateBook("日常账本")
-    driver.CreateTestBill(book)
-    house_group = driver.CreateAccountGroup("住房基金", "用于住房支出")
-    daily_group = driver.CreateAccountGroup("日常开支", "日常消费使用")
-    driver.CreateAccount("工商银行储蓄卡", False, house_group)
-    driver.CreateAccount("浦发银行信用卡", True, daily_group)
+
+    InitDatabase(driver)
 
     db.close()
--- DatabaseModel.py
+++ DatabaseModel.py
@@ -9,33 +9,60 @@ class BaseModel(Model):
         database = db
 
 class User(BaseModel):
-    email = CharField(max_length = 30, unique = True)
-    password = CharField(max_length = 64)
-    nickname = CharField(max_length = 30)
+    email = CharField(max_length=30, unique=True)
+    password = CharField(max_length=64)
+    nickname = CharField(max_length=30)
 
 class Book(BaseModel):
-    book_name = CharField(max_length = 20, unique = True)
-    is_default = BooleanField(default = False)
-    user_id = ForeignKeyField(User, backref = "books")
+    book_name = CharField(max_length=20, unique=True)
+    is_default = BooleanField(constraints=[SQL("DEFAULT False")])
+    user_id = ForeignKeyField(User, backref="books")
 
 class Bill(BaseModel):
-    amount = FloatField(default = 0)
-    inout_type = CharField(default = "支出", max_length = 10)
-    created_datetime = DateTimeField(null = True, default = datetime.datetime.now)
-    billing_date = DateField(null = True)
-    billing_time = TimeField(null = True)
-    comments = CharField(null = True, max_length = 50)
-    book_id = ForeignKeyField(Book, backref = "bills")
-    user_id = ForeignKeyField(User, backref = "bills")
+    amount = FloatField(constraints=[Check("amount >= 0.0"), SQL("DEFAULT 0.0")])
+    inout_type = CharField(max_length=10, constraints=[Check("inout_type='支出' OR inout_type='收入' OR inout_type='转账'"),
+                                                       SQL("DEFAULT '支出'")])
+    created_datetime = DateTimeField(null=True, constraints=[SQL("DEFAULT CURRENT_TIMESTAMP")])
+    billing_date = DateField(null=True)
+    billing_time = TimeField(null=True)
+    comments = CharField(null=True, max_length=50)
+    book_id = ForeignKeyField(Book, backref="bills")
+    user_id = ForeignKeyField(User, backref="bills")
 
 class AccountGroup(BaseModel):
-    account_group_name = CharField(max_length = 30)
-    user_id = ForeignKeyField(User, backref = "accountgroups")
-    comments = CharField(null = True, max_length = 50)
+    account_group_name = CharField(max_length=30)
+    comments = CharField(null=True, max_length=50)
+    user_id = ForeignKeyField(User, backref="accountgroups")
 
 class Account(BaseModel):
-    account_name = CharField(max_length = 30)
-    is_credit = BooleanField(default = False)
-    user_id = ForeignKeyField(User, backref = "accounts")
-    account_group_id = ForeignKeyField(AccountGroup, backref = "accounts")
+    account_name = CharField(max_length=30)
+    is_credit = BooleanField(constraints=[SQL("DEFAULT False")])
+    init_balance = FloatField(constraints=[Check('init_balance >= 0.0'),
+                                             SQL("DEFAULT 0.0")])
+    remain_balance = FloatField(constraints=[Check('remain_balance >= 0.0'),
+                                               SQL("DEFAULT 0.0")])
+    currency = CharField(constraints=[Check("currency='RMB' OR currency='Dollar'"),
+                                      SQL("DEFAULT 'RMB'")])
+    user_id = ForeignKeyField(User, backref="accounts")
+    account_group_id = ForeignKeyField(AccountGroup, backref="accounts")
+
+class AccountStatMonth(BaseModel):
+    date = DateField()
+    account_id = ForeignKeyField(Account, backref="AccountStatMonths")
+    amount = FloatField(constraints=[ SQL("DEFAULT 0.0")],
+                        help_text="每月余额")
+    adjust = FloatField(constraints=[SQL("DEFAULT 0.0")],
+                        help_text="每月月初调整额度")
+    interest_income = FloatField(constraints=[SQL("DEFAULT 0.0")],
+                                 help_text="每月利息收入")
+    invest_income = FloatField(constraints=[SQL("DEFAULT 0.0")],
+                               help_text="每月投资收入")
+    normal_income = FloatField(constraints=[Check("normal_income >= 0.0"),
+                                            SQL("DEFAULT 0.0")],
+                               help_text="每月普通收入，如工资")
+    normal_outcome = FloatField(constraints=[Check("normal_outcome >= 0.0"),
+                                             SQL("DEFAULT 0.0")],
+                                help_text="每月普通支出")
+    transfer = FloatField(constraints=[SQL("DEFAULT 0.0")],
+                          help_text="每月账户间转账额度")
 
